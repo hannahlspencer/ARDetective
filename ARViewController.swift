7df5c4e4898eae0eaec6704102e0b13d3645387d
Hannah Spencer <hannah.spencer@bbc.co.uk>
2020-01-25 17:22:08 +0000

    Initial commit

---
+++ ARViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ARViewController.swift
+//  ARDetective
+//
+//  Created by Hannah Spencer on 25/01/2020.
+//  Copyright © 2020 Hannah Spencer. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ARViewController: UIViewController {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.sceneView.session.run(configuration)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ARDetective
+//
+//  Created by Hannah Spencer on 25/01/2020.
+//  Copyright © 2020 Hannah Spencer. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
